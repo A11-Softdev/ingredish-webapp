b13444a06ae0354a2d2103563552424b62a1b31f
Nattharika <Nattharika25kata@gmail.com>
2024-09-12 22:30:47 +0700

    fix : confict after merge

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,5 @@
 // app/providers.tsx
-
+//add cmd tp use NextUI: npm install @nextui-org/react framer-motion 
 import {NextUIProvider} from '@nextui-org/react'
 
 export function Providers({children}: { children: React.ReactNode }) {
