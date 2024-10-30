2a97170f6e0e48dd8587fe1c86b76517805592d4
Nattharika <Nattharika25kata@gmail.com>
2024-10-30 21:32:11 +0700

    integrate

---
+++ src/app/feedHome/types/LongCardTypes.ts
@@ -0,0 +1,23 @@
+export interface LongCardDataProps {
+    _id:string;
+    user:string;
+    name:string;
+    Role:string;
+    image_url?:string;
+    serve:number;
+    ingredient:string[];
+    kitchentools:string[];
+    recipe:string[];
+    review:string[];
+    createdAt:Date;
+    IsGenerated?:boolean;
+    source:string;
+    time:string;
+}
+
+export interface BlogsProps{
+  data: LongCardDataProps[];
+  total:number;
+  page:number;
+  limit:number;
+}
